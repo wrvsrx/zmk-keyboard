cd3beb3c4ec61c87e0e3fa46987c1ab6bccfd0d7
wrvsrx <wrvsrx@outlook.com>
2025-05-05 03:37:09 +0800

    chore(env): add west and west2nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "zmk keyboard";
+
+  inputs = {
+    nixpkgs.url = "github:wrvsrx/nixpkgs/patched-nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nur-wrvsrx = {
+      url = "github:wrvsrx/nur-packages";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    west2nix = {
+      url = "github:wrvsrx/west2nix/patched-master";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.zephyr-nix.follows = "zephyr-nix";
+    };
+    zephyr-nix = {
+      url = "github:adisbladis/zephyr-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { inputs, ... }:
+      {
+        systems = [ "x86_64-linux" ];
+        perSystem =
+          { pkgs, system, ... }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.nur-wrvsrx.overlays.default
+                (final: prev: {
+                  inherit (inputs.west2nix.lib.mkWest2nix { pkgs = prev; })
+                    mkWest2nixHook
+                    ;
+                  zephyr = inputs.zephyr-nix.packages.${system};
+                })
+              ];
+            };
+            devShells.default = pkgs.mkShell {
+              nativeBuildInputs = [
+                inputs.west2nix.packages.${system}.default
+                (pkgs.python3.withPackages (ps: with ps; [ west ]))
+              ];
+            };
+            formatter = pkgs.nixfmt-rfc-style;
+          };
+      }
+    );
+}

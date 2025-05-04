c1c51863e665da49200e3c3e856bab5b79536f7b
wrvsrx <wrvsrx@outlook.com>
2025-05-05 04:55:29 +0800

    chore(scripts): add a script to test build

---
+++ scripts/test-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+Zephyr_DIR=zephyr/share/zephyr-package/cmake west build -s $PWD/zmk/app -b "eyelash_sofle_left" -S studio-rpc-usb-uart -- -DZMK_CONFIG=$PWD/config "-DSHIELD=nice_view" -DCONFIG_ZMK_STUDIO=y -DCONFIG_ZMK_STUDIO_LOCKING=n -DZMK_EXTRA_MODULES=$PWD/externals/zmk-sofle

feb8fbadda45efec7f293ccc34019ab14720b36c
wrvsrx <wrvsrx@outlook.com>
2025-09-02 19:42:27 +0800

    chore(env): use latest zephyr-nix

--- flake.nix
+++ flake.nix
@@ -20,10 +20,7 @@
       inputs.zephyr-nix.follows = "zephyr-nix";
     };
     zephyr-nix = {
-      # pin zephyr-nix to old version
-      # reason: zephyr-sdk, zephyr should match. zmk currently use zephyr 3.5.0+zmkfixes, which is not compatible with zephyr sdk >= 0.17.1
-      # so we pin zephyr-nix to this commit to avoid upgrading zephyr sdk to 0.17.2
-      url = "github:nix-community/zephyr-nix/5ba6564b7f2db1508bcf87f0f869b8e4f8be96c1";
+      url = "github:nix-community/zephyr-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
